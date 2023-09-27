d35d35cc376ad2127070507f5d6d09d324c93b9b
Aditya Putra <33738081+akari23@users.noreply.github.com>
2023-09-27 09:45:08 +0700

    Push scene to take picture

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Singleton<Instance> : MonoBehaviour where Instance : Singleton<Instance>
+{
+    public static Instance instance;
+    [Header("Singleton Instance"), SerializeField, Tooltip("Dont destroy on load?")] private bool isPersistant;
+
+    public virtual void Awake()
+    {
+        if (isPersistant)
+        {
+            if (!instance)
+            {
+                instance = this as Instance;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            instance = this as Instance;
+        }
+    }
+}
